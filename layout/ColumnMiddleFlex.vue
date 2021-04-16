1ea1378190e45fccd998523c76816cc2afec07b9
clarkliang <clarkliang@tencent.com>
2021-04-16 14:53:30 +0800

    feat(layout): 新增布局组件

---
+++ layout/ColumnMiddleFlex.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="layout-container">
+    <div class="fixed-top">
+      <slot name="fixed-top"></slot>
+    </div>
+    <div class="flex-content">
+      <slot name="flex-content"></slot>
+    </div>
+    <div class="fixed-bottom">
+      <slot name="fixed-bottom"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColumnMiddleFlex',
+};
+</script>
+
+<style lang="less" scoped>
+.layout-container {
+  display: flex;
+
+  flex-direction: column;
+
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+
+  > .fixed-top {
+    flex-shrink: 0;
+  }
+
+  > .flex-content {
+    overflow: hidden;
+    overflow-y: scroll;
+
+    flex-grow: 1;
+  }
+
+  > .fixed-bottom {
+    flex-shrink: 0;
+  }
+}
+</style>
