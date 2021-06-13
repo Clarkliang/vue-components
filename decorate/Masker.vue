0e1b7a7a70cc4514674d3623be235453207926e2
clarkliang <clarkliang@tencent.com>
2021-06-13 14:43:39 +0800

    feat(masker): 新增masker组件

---
+++ decorate/Masker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mask" :style="{ backgroundColor }" @touchmove.prevent></div>
+</template>
+
+<script>
+export default {
+  name: 'Masker',
+  props: {
+    backgroundColor: {
+      type: String,
+      default: 'rgba(0, 0, 0, 0.64)',
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.mask {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 9;
+
+  background-color: rgba(0, 0, 0, 0.64);
+}
+</style>
